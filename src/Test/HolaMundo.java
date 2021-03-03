86ee82f59c4c9f4d3f10b2dda8ffe1dacce2880f
luisn <luisn@LAPTOP-PUBDB32E>
2021-03-03 01:07:38 -0600

    version2

--- src/Test/HolaMundo.java
+++ src/Test/HolaMundo.java
@@ -8,7 +8,7 @@ package Test;
  *
  * @author luisn
  */
-public class HolaMundo {
+public class HolaMundo { 
     
     public static void main(String[] args) {
         System.out.println("Bienvenidos a la Clase");

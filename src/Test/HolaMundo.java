f28966394ff492e24abe59f8c0af5149b4ed01eb
luisn <luisn@LAPTOP-PUBDB32E>
2021-03-03 01:06:47 -0600

    version2

---
+++ src/Test/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+Nombre: Luis Javier Navarrete Cruz
+Matricula: 2022235
+ */
+package Test;
+
+/**
+ *
+ * @author luisn
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a la Clase");
+    }
+    
+}
